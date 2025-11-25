30b676755a1d22325bf2a986c950f0fbd82bb97d
alapierre-ops <drearyfying@outlook.fr>
2025-11-25 09:44:02 +0100

    Ajout de schémas de validation Zod

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,5 @@
+export * from './badge'
+export * from './habit'
+export * from './reward'
+export * from './shared'
+export * from './user'
---
+++ src/lib/validation/reward.ts
@@ -0,0 +1,25 @@
+import { RewardType } from '@prisma/client'
+import { z } from 'zod'
+
+import { cuidSchema, goldSchemaBase, iconSchema } from './shared'
+
+const rewardBaseSchema = z.object({
+  title: z.string().min(1).max(80),
+  cost: goldSchemaBase.min(0).max(5_000),
+  type: z.enum(RewardType),
+  icon: iconSchema,
+})
+
+export const rewardCreateSchema = rewardBaseSchema.extend({
+  creatorId: cuidSchema.optional().nullable(),
+})
+
+export const userRewardCreateSchema = z.object({
+  userId: cuidSchema,
+  rewardId: cuidSchema,
+  isConsumed: z.boolean().optional(),
+})
+
+export const rewardIdentifierSchema = z.object({
+  rewardId: cuidSchema,
+})
