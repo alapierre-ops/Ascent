1652335d8208dc55019e401f17aab5ce5a96ca02
mbert30 <bert.matheo2521@gmail.com>
2026-06-11 13:33:27 +0200

    Ajout trustHost pour le déploiement

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import { verifyPassword } from '@/lib/password'
 import { prisma } from '@/lib/prisma'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   session: {
     strategy: 'jwt',
   },
